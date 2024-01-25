1d2f723786b4aa5d66cad8fab38accf36466a22a
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-25 19:07:10 +0530

    Add Logic to CartContainer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import { useSelector } from 'react-redux';
 import { CartIcon } from '../icons';
 const Navbar = () => {
+  // can use  'store' intead of 'state' as well to understand that we are aceesing whole store
   const { amount } = useSelector((store) => store.cart);
   console.log(amount);
   return (
     <nav>
       <div className="nav-center">
         <h3>redux toolkit</h3>
+        <div className="nav-container">
+          <CartIcon></CartIcon>
+          <div className="amount-container">
+            <p className="total-amount">{amount}</p>
+          </div>
+        </div>
       </div>
     </nav>
   );
